b7b212c1c5917e1c79f507a944b0b1571d97c53b
grayemy216_svg <gray.emy216@gmail.com>
2026-04-01 09:12:45 -0400

    adding changes

---
+++ src/script.js
@@ -0,0 +1,18 @@
+// alert("Hello, World!");""
+
+// let x =2;
+
+
+// let age;   //Declaring a variable
+// age = 30;    //Intializing a variable
+// let username = prompt("What is your name?");
+// alert("hello" + username);
+
+// let sentence = "I" + " am "  + " here";
+// alert(sentence);
+
+let username = prompt("what is your name?");
+let age = prompt("what is your age?");
+let email = prompt("what is your email?");
+
+alert("hi " + username + ", you are " + age + " years old and your email is " + email);
